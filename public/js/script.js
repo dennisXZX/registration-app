293af3b8d8ce3a51c580fa528ae7948b26f557a1
Dennis Xiao <dennisboys@gmail.com>
2016-12-15 21:10:58 +1100

    added form validation

--- public/js/script.js
+++ public/js/script.js
@@ -10,15 +10,23 @@ $(document).ready(function(){
         const nationality = $("#nationality").val();
         const occupation = $("#occupation").val();
 
-        // send a post request to register-success page
-        $.ajax({
-            method: "POST",
-            url: "register-success",
-            data: { name: name, nationality: nationality, occupation: occupation}
+        if(name == "") {
+            $("#name").css("border-color", "red");
+        } else if (nationality == "") {
+            $("#nationality").css("border-color", "red");           
+        } else if (occupation == "") {
+            $("#occupation").css("border-color", "red");           
+        } else {
+            // send a post request to register-success page
+            $.ajax({
+                method: "POST",
+                url: "register-success",
+                data: { name: name, nationality: nationality, occupation: occupation}
 
-        }).done(function(result){
-                window.location.replace('register-success');
-        });
+            }).done(function(result){
+                    window.location.replace('register-success');
+            });            
+        }
 
     });
 
